ab5e2371619bd357135879186892ca88be023538
lucas.souza <lucas.souza@opensystem.srv.br>
2021-02-20 03:45:15 -0300

    adicionado rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.route';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    ContatoComponent,
+    SobreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig)]
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF,useValue:'/'} //prefixo de rota
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
